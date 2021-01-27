8e177733cb040cfa203da79e88e6bdade3baf10d
jphjs <josephjsoares@protonmail.com>
2021-01-27 18:44:20 +0000

    Backend app creation and env/docker fix

--- src/whereami/settings.py
+++ src/whereami/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = os.environ.get("SECRET_KEY")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", default=0)
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
 
 
 # Application definition
---
+++ src/whereamibackend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WhereamibackendConfig(AppConfig):
+    name = 'whereamibackend'
