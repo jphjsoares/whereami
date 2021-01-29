40b593d8151b96bf6fe2466057cc8e3c30497240
jphjs <josephjsoares@protonmail.com>
2021-01-29 15:39:31 +0000

    Map creator architecture layout

--- src/map/urls.py
+++ src/map/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="map-home"),
+    path('createcustom', views.create_custom, name="create-custom"),
+    path('createbyregion', views.create_by_region, name="create-by-region"),
+    path('createworld', views.create_world, name="create-world")
 ]
--- src/map/views.py
+++ src/map/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'map/create-map.html')
+    return render(request, 'map/index.html')
+
+def create_custom(request):
+    return render(request, 'map/create-custom.html')
+
+def create_by_region(request):
+    return render(request, 'map/create-by-region.html')
+
+def create_world(request):
+    return render(request, 'map/create-world.html')
