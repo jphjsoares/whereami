ad20e79b7644d9ab924320f294ee12fca1a00521
jphjs <josephjsoares@protonmail.com>
2021-01-31 20:23:18 +0000

    Check if there is a close image when selecting a location

--- src/static/map/javascript/mapCreation.js
+++ src/static/map/javascript/mapCreation.js
@@ -1,3 +1,16 @@
+
+/*
+TO FIND IMAGES CLOSE TO COORDINATES
+https://www.mapillary.com/developer/api-documentation/#search-images
+https://a.mapillary.com/v3/images?client_id=MGNWR1hFdWVhb3FQTTJxcDZPUExHZzo2NTE4YmM3NmY0YWYyNGYy&closeto=-122.3079,47.6537 (lng,lat) (RADIUS IS 100m, by default)
+*/
+/*
+TO DELETE A MARKER, JUST USE markersCoords.markers[i].remove()
+AND FOR THE JSON
+delete markersaCoords.markers[i]
+*/
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2FzZWlubWVsbCIsImEiOiJja2tpZWU3bG8wNXN4MnBzNzVibnN5dG90In0.D6Y43QmUBiirztruQeEFHA';
 let map = new mapboxgl.Map({
     container: 'map',
@@ -5,6 +18,12 @@ let map = new mapboxgl.Map({
     center: [-9.136068933525848, 38.74608203665869], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+
+/**
+ * 
+ * GLOBAL VARIABLES
+ * 
+ */
 
 let i = 0;
 
@@ -20,15 +39,16 @@ let markersCoords = {
 
 let table = document.getElementById("show-coords");
 
-/*
-TO FIND IMAGES CLOSE TO COORDINATES
-https://www.mapillary.com/developer/api-documentation/#search-images
-*/
-/*
-TO DELETE A MARKER, JUST USE markersCoords.markers[i].remove()
-AND FOR THE JSON
-delete markersaCoords.markers[i]
-*/
+const isThereACloseImage = async (lng, lat) => {
+    //TODO: find a way to hide client id
+    const url = "https://a.mapillary.com/v3/images?client_id=MGNWR1hFdWVhb3FQTTJxcDZPUExHZzo2NTE4YmM3NmY0YWYyNGYy&closeto=" + lng + "," + lat;
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+    
+}
+
+
 
 function deleteSelection(indexToDelete) {
     //Instead of using delete, set it to an empty string and filter out on the backend
@@ -88,21 +108,31 @@ map.on('style.load', function() {
     });
     
     map.on('click', function(e){
-        
-        chosenCoords["coordinates"].push(e.lngLat.wrap()); // Add all the coordinates to a json object
-        
-        var marker = new mapboxgl.Marker()
-        .setLngLat([chosenCoords.coordinates[i].lng, chosenCoords.coordinates[i].lat])
-        .addTo(map);
 
-        markersCoords["markers"].push(marker); // Add every marker to a marker json object
+        //Check if there is a close image on the selected location
+        verifyCloseImage = isThereACloseImage(e.lngLat.wrap().lng, e.lngLat.wrap().lat).then(res => {
+            //TODO: Show the user a loading message while fetching mapillary api
+            if(res.features.length === 0) {
+                alert("No close image, try another location!");
+                
+            } else {
+                chosenCoords["coordinates"].push(e.lngLat.wrap()); // Add all the coordinates to a json object
         
-        //Add elements to table
-        populateTable(i);
-
-        //FOR DEBUG PORPUSES ONLY
-        //console.log(chosenCoords["coordinates"]);
-        i++;
+                var marker = new mapboxgl.Marker()
+                .setLngLat([chosenCoords.coordinates[i].lng, chosenCoords.coordinates[i].lat])
+                .addTo(map);
+        
+                markersCoords["markers"].push(marker); // Add every marker to a marker json object
+                
+                //Add elements to table
+                populateTable(i);
+        
+                //FOR DEBUG PORPUSES ONLY
+                //console.log(chosenCoords["coordinates"]);
+                i++;
+            }
+        });
+        
     });
 
 });
