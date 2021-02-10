7712384eba051ad0a09e05f48061417574438102
sleetynine018 <josephjsoraes@gmail.com>
2021-02-10 18:55:02 +0000

    First page update

--- src/game/urls.py
+++ src/game/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="game-home"),
+    path('singleplayer', views.singleplayer, name="singleplayer"),
+    path('multiplayer', views.multiplayer, name="multiplayer")
 ]
