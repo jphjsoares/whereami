3f09b75e66b7cb00385fdd556c438f0a5c91db36
sleetynine018 <josephjsoraes@gmail.com>
2021-02-18 21:36:42 +0000

    Made create by region faster and enabled loading icon on chrome

--- src/static/map/javascript/mapByRegion.js
+++ src/static/map/javascript/mapByRegion.js
@@ -28,67 +28,72 @@ map.on('draw.update', checkForPolygons);
 
 
 function checkForPolygons() {
-	let polygon = draw.getAll().features;
-	if(polygon.length > 0) {
-		document.getElementById("submit-button").disabled = false;
-	} else {
-		document.getElementById("submit-button").disabled = true;
-	}		
+    let polygon = draw.getAll().features;
+    if(polygon.length > 0) {
+        document.getElementById("submit-button").disabled = false;
+    } else {
+        document.getElementById("submit-button").disabled = true;
+    }       
 }
 
 
 function buildUrl( box0, box1, box2, box3, lng, lat) {
-	//Be careful with radius, might generate a point out of polygon, requires more testing
-	return "https://a.mapillary.com/v3/images?bbox=" + box0 + ',' + box1 + ',' + box2 + ',' + box3 + "&closeto=" + lng + ',' + lat + "&radius=50000&per_page=1&client_id=MGNWR1hFdWVhb3FQTTJxcDZPUExHZzo2NTE4YmM3NmY0YWYyNGYy";
+    //Be careful with radius, might generate a point out of polygon, requires more testing
+    return "https://a.mapillary.com/v3/images?bbox=" + box0 + ',' + box1 + ',' + box2 + ',' + box3 + "&closeto=" + lng + ',' + lat + "&radius=50000&per_page=1&client_id=MGNWR1hFdWVhb3FQTTJxcDZPUExHZzo2NTE4YmM3NmY0YWYyNGYy";
 }
 
 function generateRandomPointsOnRegion(polygon) {
 
-	let polyBbox = turf.bboxPolygon(turf.bbox(polygon))
-	let points = turf.randomPoint(1, polyBbox);
+    let polyBbox = turf.bboxPolygon(turf.bbox(polygon))
+    let points = turf.randomPoint(1, polyBbox);
 
-	let newUrl = buildUrl(polyBbox["bbox"][0], polyBbox["bbox"][1], polyBbox["bbox"][2], polyBbox["bbox"][3], 
-		points["features"][0]["geometry"]["coordinates"][0], 
-		points["features"][0]["geometry"]["coordinates"][1]);
+    let newUrl = buildUrl(polyBbox["bbox"][0], polyBbox["bbox"][1], polyBbox["bbox"][2], polyBbox["bbox"][3], 
+        points["features"][0]["geometry"]["coordinates"][0], 
+        points["features"][0]["geometry"]["coordinates"][1]);
 
-	$.ajaxSetup({
-		async: false
-	});
-	$.get(newUrl, function(data) {
-		if(data.features.length !== 0 && turf.booleanPointInPolygon(points["features"][0]["geometry"], polygon)) {			
-			//console.log(data["features"][0]["properties"].key) FOR DEBUG
-			coordinatesToSubmit.push(data["features"][0]["properties"].key);
-		} else {
-			//console.error("No close image found, trying again!")  FOR DEBUG
-			generateRandomPointsOnRegion(polygon);
-		}
-		
-	})
+    $.get(newUrl, function(data) {
+        if(data.features.length !== 0 && turf.booleanPointInPolygon(points["features"][0]["geometry"], polygon)) {          
+            coordinatesToSubmit.push(data["features"][0]["properties"].key);
+        } else {
+            generateRandomPointsOnRegion(polygon);
+        }
+    });
+}
+
+function getReadyForSubmit() {
+    for(locationIndex = 0; locationIndex < coordinatesToSubmit.length; locationIndex++) {
+        //Every line on the text area will be in the form lng,lat
+        //On the backend we must get every line and separate by comma and make an array for each pair
+        document.getElementById("locations-to-submit").value += coordinatesToSubmit[locationIndex] + '\n';
+    }
+    $('form').unbind('submit').submit();
+}
+
+function checkIfPopulated() {
+    let timer = window.setInterval(function(){
+        if (coordinatesToSubmit.length == 10) {
+            window.clearInterval(timer);
+            console.log(coordinatesToSubmit);
+            getReadyForSubmit();
+        }
+    }, 50);
 }
 
 
-
-
-function handleClick() {
- 	let polygon = draw.getAll().features;
-	document.getElementById("slow-alert").remove();
-	document.getElementById("main-content").style.display = "none";
+$("#form").on('submit', function(e) {
+    e.preventDefault();
+    let polygon = draw.getAll().features;
+    document.getElementById("main-content").style.display = "none";
     document.getElementById("loading").style.display = "block";
     document.getElementById("loading-text").style.display = "block";
+    console.log("Starting....")
 
-	for (numOfPolygons = 0; numOfPolygons < polygon.length; numOfPolygons++) {
-		
-		//Number of points
-		for(i = 0; i < 10; i++) {
-			generateRandomPointsOnRegion(polygon[numOfPolygons]);
-
-		}
-	}
-
-	for(locationIndex = 0; locationIndex < coordinatesToSubmit.length; locationIndex++) {
-		//Every line on the text area will be in the form lng,lat
-		//On the backend we must get every line and separate by comma and make an array for each pair
-		document.getElementById("locations-to-submit").value += coordinatesToSubmit[locationIndex] + '\n';
-	}
-
-}
+    for (numOfPolygons = 0; numOfPolygons < polygon.length; numOfPolygons++) {
+        
+        //Number of points
+        for(let i = 0; i < 10; i++) {
+            generateRandomPointsOnRegion(polygon[numOfPolygons]);
+        }
+        checkIfPopulated();
+    }
+});
