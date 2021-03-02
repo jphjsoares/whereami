a20c564c335a3c662f619819d3e3b1f48a514432
sleetynine018 <josephjsoraes@gmail.com>
2021-03-02 01:19:03 +0000

    Change database creds for production

--- src/whereami/settings.py
+++ src/whereami/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'whereami.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -104,6 +104,19 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': 5432,
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
