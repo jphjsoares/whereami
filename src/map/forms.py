4ec2dfce8f7a029a14588ff620a7ff858bddca9d
José Soares <josephjsoraes@gmail.com>
2021-03-18 12:27:01 +0000

    Start of documentation and refactoring of map app

--- src/map/forms.py
+++ src/map/forms.py
@@ -3,19 +3,67 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 def clean_numoflocations(value):
-        if value > 100:
-            raise ValidationError(_("Too many locations."))
-        if value < 5:
-            raise ValidationError(_("You need at least 5 locations"))
-        return value
+    """Map world validator.
+
+    This functions validates the submitted number of locations in the 
+    random map world generator
+    
+    Parameters
+    ----------
+    value : 
+        Submitted number of locations
+
+    Raises
+    -----
+    ValidationError : 
+        If the submitted number of locations is above 100 or below 5  
+    """
+
+    if value > 100:
+        raise ValidationError(_("Too many locations."))
+    if value < 5:
+        raise ValidationError(_("You need at least 5 locations"))
+    return value
 
 def clean_locations(locations):
+    """Map region validator.
+
+    This functions validates the submitted number of locations in the 
+    random map region generator
+    
+    Parameters
+    ----------
+    locations : 
+        Array containing the coordinates randomly generated
+
+    Raises
+    -----
+    ValidationError : 
+        If the number of locations is not exactly 10  
+    """
+
     locs_split = locations.splitlines()
     if len(locs_split) != 10:
         raise ValidationError(_("Something went wrong"))
     return locations
 
 def clean_locationscustom(locations):
+    """Custom map validator.
+
+    This functions validates all the custom selections submitted by a map
+    creator
+
+    Parameters
+    ----------
+    locations : 
+        Array containing the coordinates chosen by a map creator
+
+    Raises
+    -----
+    ValidationError : 
+        If the number of locations is below 5 or above 100
+    """    
+    
     locs_split = locations.splitlines()
     if len(locs_split) < 5:
         raise ValidationError(_("Please select more than 5 locations"))
--- src/map/models.py
+++ src/map/models.py
@@ -2,25 +2,33 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 def make_hash_id():
+    """Creates the random map hash with letters and digits"""
     import random, string
     allowed_chars = ''.join((string.ascii_letters, string.digits))
     return ''.join(random.choices(allowed_chars, k=12))
 
 
 class Map(models.Model):
+    """ Map model
+
+    Fields
+    ----------
+    hash_id : CharField
+        Used to identify each map
+    map_type : IntegerField
+        1- Custom
+        2- Region
+        3- World
+    num_of_locations : IntegerField
+        The number of locations a map has
+    mapillary_image_key : ArrayField(CharField)
+        Array containing all the keys to each image in the game
+    """
+
     hash_id = models.CharField(max_length=13, default=make_hash_id)
-    """
-    MAP TYPES:
-    1 - custom
-    2 - region
-    3 - world
-    """
     map_type = models.IntegerField(default=1)
-    name = models.CharField(max_length=50)
-    creator = models.CharField(max_length=40)
     num_of_locations = models.IntegerField(default=0)    
     mapillary_image_key = ArrayField(models.CharField(max_length=25))    
-    times_played = models.IntegerField(default=0)
 
     def __str__(self):
         return '%s, %s, %s' % (self.name, self.creator, self.mapillary_image_key)
@@ -30,12 +38,18 @@ class Map(models.Model):
         return reverse('get-map',args=[str(self.hash_id)])
 
 class ReportedImages(models.Model):
+    """ Game instance model
+
+    Fields
+    ----------
+    mapillary_image : CharField
+        Key of the reported image
+    reason_is_low_quality : IntegerField
+        Is 1 if image was reported because of low quality
+    reason_is_wrong_coordinates : IntegerField
+        Is 1 if image was reported because of containing the wrong coordinates
+    """
+
     mapillary_image = models.CharField(max_length=25)
-    """
-    REPORT REASONS:
-    0 - no reason given
-    1 - image related
-    2 - image location related
-    """
     reason_is_low_quality = models.IntegerField(default=0)
     reason_is_wrong_coordinates = models.IntegerField(default=0)
